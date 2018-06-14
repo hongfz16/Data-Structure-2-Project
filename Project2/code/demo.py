ce528227da7fe6de5abb2118e9986ef0468278f1
LeeDa16 <lishuai16THU@163.com>
2018-06-14 14:38:13 +0800

    Update demo.py

--- Project2/code/demo.py
+++ Project2/code/demo.py
@@ -29,48 +29,27 @@ args = parser.parse_args()
 
 root = './list/'
 
-transform_train = transforms.Compose(
+transform_db = transforms.Compose(
     [transforms.CenterCrop(227),
      transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-transform_test = transforms.Compose(
+transform_query = transforms.Compose(
     [transforms.CenterCrop(227),
      transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-trainset = MyDataset(txt=root+'trainlist.txt',transform=transform_train)
-testset = MyDataset(txt=root+'testlist.txt',transform=transform_test)
+dbset = MyDataset(txt=root+'trainlist.txt',transform=transform_db)
+queryset = MyDataset(txt=root+'testlist.txt',transform=transform_query)
 
 
 def load_local_data():
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
+    dbloader = torch.utils.data.DataLoader(dbset, batch_size=100,
                                               shuffle=False, num_workers=2)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+    queryloader = torch.utils.data.DataLoader(queryset, batch_size=100,
                                              shuffle=False, num_workers=2)
-    return trainloader, testloader
+    return dbloader, queryloader
 
-
-def load_data():
-    transform_train = transforms.Compose(
-        [transforms.Resize(227),
-         transforms.CenterCrop(227),
-         transforms.ToTensor(),
-         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-    transform_test = transforms.Compose(
-        [transforms.Resize(227),
-         transforms.CenterCrop(227),
-         transforms.ToTensor(),
-         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-    trainset = MyDataset(txt='./list/trainlist.txt', transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
-                                              shuffle=False, num_workers=2)
-
-    testset = MyDataset(txt='./list/testlist.txt', transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                             shuffle=False, num_workers=2)
-    return trainloader, testloader
-
-
+  
 def binary_output(dataloader):
     net = AlexNetPlusLatent(args.bits)
     net.load_state_dict(torch.load('./model/86.7'))
@@ -91,27 +70,27 @@ def binary_output(dataloader):
     return torch.round(full_batch_output), full_batch_label
 
 
-def precision(trn_binary, trn_label, tst_binary, tst_label):
-    trn_binary = trn_binary.cpu().numpy()
-    trn_binary = np.asarray(trn_binary, np.int32)
-    trn_label = trn_label.cpu().numpy()
-    tst_binary = tst_binary.cpu().numpy()
-    tst_binary = np.asarray(tst_binary, np.int32)
-    tst_label = tst_label.cpu().numpy()
-    query_times = tst_binary.shape[0]
-    trainset_len = train_binary.shape[0]
+def precision(db_binary, db_label, qry_binary, qry_label):
+    db_binary = db_binary.cpu().numpy()
+    db_binary = np.asarray(db_binary, np.int32)
+    db_label = db_label.cpu().numpy()
+    qry_binary = qry_binary.cpu().numpy()
+    qry_binary = np.asarray(qry_binary, np.int32)
+    qry_label = qry_label.cpu().numpy()
+    query_times = qry_binary.shape[0]
+    dbset_len = db_binary.shape[0]
     AP = np.zeros(query_times)
-    Ns = np.arange(1, trainset_len + 1)
+    Ns = np.arange(1, dbset_len + 1)
     total_time_start = time.time()
     for i in range(query_times):
         print('Query ', i+1)
-        query_label = tst_label[i]
-        query_binary = tst_binary[i,:]
-        query_result = np.count_nonzero(query_binary != trn_binary, axis=1)  # hamming distance
+        query_label = qry_label[i]
+        query_binary = qry_binary[i,:]
+        query_result = np.count_nonzero(query_binary != db_binary, axis=1)  # hamming distance
         sort_indices = np.argsort(query_result)  # sort by hamming distance
         print(query_label)
         print(sort_indices[:10])
-        buffer_yes= np.equal(query_label, trn_label[sort_indices]).astype(int)
+        buffer_yes= np.equal(query_label, db_label[sort_indices]).astype(int)
         P = np.cumsum(buffer_yes) / Ns
         AP[i] = np.sum(P * buffer_yes) /sum(buffer_yes)
     map = np.mean(AP)
@@ -119,50 +98,50 @@ def precision(trn_binary, trn_label, tst_binary, tst_label):
     print('total query time = ', time.time() - total_time_start)
 
 
-def single_query(query_binary,label,trn_binary,trn_label):
-    trn_binary = trn_binary.cpu().numpy()
-    trn_binary = np.asarray(trn_binary, np.int32)
-    trn_label = trn_label.cpu().numpy()
+def single_query(query_binary,label,db_binary,db_label):
+    db_binary = db_binary.cpu().numpy()
+    db_binary = np.asarray(db_binary, np.int32)
+    db_label = db_label.cpu().numpy()
     curr_time=time.time()
-    query_result=np.count_nonzero(query_binary!=trn_binary,axis=1)
+    query_result=np.count_nonzero(query_binary!=db_binary,axis=1)
     sort_indices=np.argsort(query_result)
     print(sort_indices[:10])
     return sort_indices[:10]
 
 if __name__ == '__main__':
-    if os.path.exists('./result/train_binary') and os.path.exists('./result/train_label') and \
-        os.path.exists('./result/test_binary') and os.path.exists('./result/test_label') and args.pretrained == 0:
-        train_binary = torch.load('./result/train_binary')
-        train_label = torch.load('./result/train_label')
-        test_binary = torch.load('./result/test_binary')
-        test_label = torch.load('./result/test_label')
+    if os.path.exists('./result/db_binary') and os.path.exists('./result/db_label') and \
+        os.path.exists('./result/query_binary') and os.path.exists('./result/query_label') and args.pretrained == 0:
+        db_binary = torch.load('./result/db_binary')
+        db_label = torch.load('./result/db_label')
+        query_binary = torch.load('./result/query_binary')
+        query_label = torch.load('./result/query_label')
 
     else:
-        trainloader, testloader = load_local_data()
-        train_binary, train_label = binary_output(trainloader)
-        test_binary, test_label = binary_output(testloader)
+        dbloader, queryloader = load_local_data()
+        db_binary, db_label = binary_output(dbloader)
+        query_binary, query_label = binary_output(queryloader)
         if not os.path.isdir('result'):
             os.mkdir('result')
-        torch.save(train_binary, './result/train_binary')
-        torch.save(train_label, './result/train_label')
-        torch.save(test_binary, './result/test_binary')
-        torch.save(test_label, './result/test_label')
+        torch.save(db_binary, './result/db_binary')
+        torch.save(db_label, './result/db_label')
+        torch.save(query_binary, './result/qry_binary')
+        torch.save(query_label, './result/qry_label')
 
-    tst_binary = test_binary.cpu().numpy()
-    tst_binary = np.asarray(test_binary, np.int32)
-    tst_label = test_label.cpu().numpy()
+    qry_binary = query_binary.cpu().numpy()
+    qry_binary = np.asarray(query_binary, np.int32)
+    qry_label = query_label.cpu().numpy()
 
-    precision(train_binary, train_label, test_binary, test_label) # evaluate by mAP
+    precision(db_binary, db_label, query_binary, query_label) # evaluate by mAP
 
     # show images
     num=888
-    ans=single_query(tst_binary[num],tst_label[num],train_binary,train_label)
-    test_fn,label=testset.imgs[num]
-    img=Image.open('./data/image/'+test_fn).convert('RGB')
+    ans=single_query(qry_binary[num],qry_label[num],db_binary,db_label)
+    query_fn,label=queryset.imgs[num]
+    img=Image.open('./data/image/'+query_fn).convert('RGB')
     plt.subplot(4,4,1)
     plt.imshow(img)  # print query image
     for i in range(len(ans)):
-        fn,label=trainset.imgs[ans[i]]
+        fn,label=dbset.imgs[ans[i]]
         img=Image.open('./data/image/'+fn).convert('RGB')
         plt.subplot(4,4,2+i)
         plt.imshow(img)  # print retrieved images
